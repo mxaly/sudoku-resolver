d2b835fa7d6836f577518633a3913bd9d415880a
Rafal Gawlik <gawlikraf@gmail.com>
2022-12-28 12:06:45 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,63 @@
+import { simple } from "@/examples";
+import { useSudoku } from "@/hooks/useSudoku";
+import { createFromString } from "@/lib/grid";
+import { Resolver } from "@/Pages/Resolver/Resolver";
+import { Modal } from "@/Components/Base/Modal/Modal";
+import { useState } from "react";
+import { Panel } from "@/Components/Base/Panel/Panel";
+import { Button } from "@/Components/Base/Button/Button";
+import { PageLayout } from "./Components/Layout/PageLayout";
+
+import { ReactComponent as CloseIcon } from "@/assets/icons/close-circle.svg";
+
 function App() {
-  return <p>TODO</p>;
+  const { solveStep, grid, optionsCount } = useSudoku(createFromString(simple));
+
+  const [modal1, setModal1] = useState(true);
+  const [modal2, setModal2] = useState(true);
+
+  const onClick = (btn: string) => {
+    console.log("onClick:", { btn });
+  };
+
+  const sizes = ["small", "medium"] as const;
+  const types = ["primary", "secondary"] as const;
+
+  let buttons = [];
+  for (const size of sizes) {
+    for (const type of types) {
+      buttons.push(
+        <>
+          <Button
+            label={`${size}-${type}`}
+            icon={<CloseIcon />}
+            onClick={() => onClick(`${size}-${type}-icon`)}
+            type={type}
+            size={size}
+          />
+          <Button
+            label={`${size}-${type}`}
+            onClick={() => onClick(`${size}-${type}`)}
+            type={type}
+            size={size}
+          />
+          <Button
+            type={type}
+            size={size}
+            onClick={() => onClick(`${size}-${type}-custom`)}
+          >
+            <p>{`${size}-${type}-custom`}</p>
+          </Button>
+        </>
+      );
+    }
+  }
+
+  return (
+    <>
+      <Resolver />
+    </>
+  );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "path";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  plugins: [react()],
+  plugins: [
+    svgr({
+      exportAsDefault: false,
+    }),
+    react(),
+  ],
 });
