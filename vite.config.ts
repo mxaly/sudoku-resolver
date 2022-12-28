38d2202f7d50b0a0fee0f69c7fe3c8360c614f44
mxaly <gawlikraf@gmail.com>
2022-12-28 12:25:08 +0100

    install and configure vite-svgr-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "path";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  plugins: [react()],
+  plugins: [svgr(), react()],
   test: {
     globals: true,
     environment: "jsdom",
