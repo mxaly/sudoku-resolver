3bfbbfed1295cfa08a4f4beec7a4d046c0dadf5b
mxaly <gawlikraf@gmail.com>
2023-01-02 00:01:16 +0100

    add basic Resolver page

---
+++ src/Pages/Resolver/Resolver.tsx
@@ -0,0 +1,17 @@
+import { Grid as GridComponent } from "@/Components/Grid/Grid";
+import { PageLayout } from "@/Components/Layout/PageLayout";
+import { simple } from "@/examples";
+import { useSudoku } from "@/hooks/useSudoku";
+import { createFromString } from "@/lib/grid";
+
+export function Resolver() {
+  const { solveStep, grid, optionsCount } = useSudoku(createFromString(simple));
+
+  return (
+    <PageLayout flexCenter>
+      <GridComponent grid={grid} />
+      <button onClick={solveStep}>solve</button>
+      <div>{optionsCount}</div>
+    </PageLayout>
+  );
+}
